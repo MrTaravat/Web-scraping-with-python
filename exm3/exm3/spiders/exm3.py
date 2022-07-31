4522550017e8cecdde4abc1442f6f599c4660fb8
mohammad taravat <taravat.mohammad@gmail.com>
2022-07-31 05:35:08 +0430

    exm3 get url tsetmc

---
+++ exm3/exm3/spiders/exm3.py
@@ -0,0 +1,11 @@
+#exm1
+import requests
+from bs4 import BeautifulSoup
+listurl=[]
+url="http://www.tsetmc.com/Loader.aspx?ParTree=111C1417" #url
+response=requests.get(url)      #response
+soup = BeautifulSoup(response.content,"html.parser")
+for a_href in soup.find_all("a", href=True):
+    listurl.append('www.tsetmc.com/'+a_href["href"])
+listurl=list(set(listurl))
+print(listurl)
