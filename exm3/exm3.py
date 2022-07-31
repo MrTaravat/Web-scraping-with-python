0f8bc4e226cfa8f2ec251ffdea9b8e997d49160d
mohammad taravat <taravat.mohammad@gmail.com>
2022-07-31 08:53:06 +0430

    exm4

--- exm3/exm3.py
+++ exm3/exm3.py
@@ -6,6 +6,6 @@ url="http://www.tsetmc.com/Loader.aspx?ParTree=111C1417" #url
 response=requests.get(url)      #response
 soup = BeautifulSoup(response.content,"html.parser")
 for a_href in soup.find_all("a", href=True):
-    listurl.append('www.tsetmc.com/'+a_href["href"])
+    listurl.append('http://www.tsetmc.com/'+a_href["href"])
 listurl=list(set(listurl))
 print(listurl)
--- exm4/exm4.py
+++ exm4/exm4.py
@@ -1,8 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-driver = webdriver.Firefox()
-driver.get('http://www.tsetmc.com/loader.aspx?ParTree=111C1412&inscode=62603302940123327')
-driver.maximize_window()
-download=driver.find_element_by_id("InsExport")
-download.click()
-
+import glob
+import os
+listurl=['http://www.tsetmc.com/loader.aspx?ParTree=111C1412&inscode=22667016906590506']
+chromeOptions = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : "/home/mamadtnt/Desktop/excel"}
+chromeOptions.add_experimental_option("prefs",prefs)
+chromedriver = "/usr/bin/chromedriver"
+driver = webdriver.Chrome(executable_path=chromedriver, options=chromeOptions)
+# driver = webdriver.Chrome('/usr/bin/chromedriver')
+for url in listurl:
+    driver.get(url)
+    driver.maximize_window()
+    btn_1=WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.ID, "InsExport")))
+    btn_1.click()
+    time.sleep(5)
+    list_of_files = glob.glob('/home/mamadtnt/Desktop/excel/*.csv', recursive=True)
+    latest_file = max(list_of_files, key=os.path.getctime)
+    os.rename(latest_file, '/home/mamadtnt/Desktop/excel/'+driver.title.split(' ')[0]+'.csv')
